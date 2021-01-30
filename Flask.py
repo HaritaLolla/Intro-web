9d99f27c097bde44f7fca8564f0c7c9b14df9ef9
Harita Lolla <39057116+HaritaLolla@users.noreply.github.com>
2021-01-30 14:08:55 +0100

    Add files via upload

---
+++ Flask.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+	
+@app.route('/index')
+def index():
+    return render_template('index.html')
+	
+@app.route('/mywebsite')
+def mywebsite():
+    return render_template('first.html')
+
+@app.route('/add')
+def add():
+
+	x = 5 
+	y = 4 
+    t = datetime.time(datetime.now())
+    return x+y, t 
+
+app.run(port=8080)
